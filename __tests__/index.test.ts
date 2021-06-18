3bc9ee25e1b7a82e7c7472b084c7c946989b92bc
David Wong <davidwong@ps.asl.com.hk>
2021-06-18 14:51:10 +0800

    adding new functions

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,25 @@
-import { getHelloWorld } from '../lib';
+import { roundToDecimal, getRandomKey, mergeObjectWithArrayOverWrite } from '../src';
 
-test('My Greeter', () => {
-    expect(getHelloWorld()).toBe('hello world');
+test('round integer to 2 decimal', () => {
+    expect(roundToDecimal(5.555)).toBe('5.56');
 });
+
+test('round integer to 2 decimal', () => {
+    expect(roundToDecimal(5.8646)).toBe('5.87');
+});
+
+test('merge three object', () => {
+    const a = { a: 1, b: { d: 5, e: 6 }, c: 3, z: ['a', 'b'] };
+    const b = { a: 2, b: { e: 7 }, z: ['c'] };
+    const c = { c: 8 };
+
+    const result = mergeObjectWithArrayOverWrite(a, b, c);
+
+    expect(result.a).toBe(2);
+    expect(result.b.d).toBe(5);
+    expect(result.b.e).toBe(7);
+    expect(result.z[0]).toBe('c');
+    expect(result.c).toBe(8);
+    expect(result.c[1]).toBeUndefined();
+});
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,113 @@
-export const getHelloWorld = () => {
-    return 'hello world';
+import merge from 'deepmerge-plus';
+
+export const sleep = async (time: number): Promise<unknown> => {
+    return new Promise((resolve) => setTimeout(resolve, time));
+};
+
+export const execShellCommand = (cmd: string) => {
+    const exec = require('child_process').exec;
+    return new Promise((resolve) => {
+        exec(cmd, (error: any, stdout: string, stderr: string) => {
+            if (error) {
+                return resolve(error);
+            }
+            resolve(stdout ? stdout : stderr);
+        });
+    });
+};
+
+export const escapeSpecialChars = function (string: string): string {
+    // eslint-disable-next-line no-useless-escape
+    return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+export const round = (value: number, precision: number) => {
+    const multiplier = Math.pow(10, precision || 0);
+    return (Math.round(value * multiplier) / multiplier).toFixed(precision);
+};
+
+export const roundToDecimal = (value: number, precision = 2): string => {
+    const temp = parseFloat(round(value, precision + 1)) + 0.000000001;
+    return round(temp, precision);
+};
+
+export const getRandomKey = (
+    length = 8,
+    charset: string | 'a-z' | 'A-Z' | 'default' | 'base32' = 'default',
+): string => {
+    // charset can be a-z, A-Z, 0-9, or base32 (A-Z and 2-7) or manually enter charset
+    // default a-zA-Z0-9
+
+    return require('rand-token')
+        .generator({
+            chars: charset,
+        })
+        .generate(length);
+};
+
+export const restrictQuery = (queryObject: Record<string, any>): Record<string, string> => {
+    const result: Record<string, string> = {};
+    const queries = Object.entries(queryObject);
+
+    for (let i = 0; i < queries.length; i++) {
+        const [key, value] = queries[i];
+
+        if (typeof value !== 'string') {
+            throw new Error('query consist of non string query');
+        }
+
+        result[key] = value;
+    }
+
+    return result;
+};
+
+export const isInternetExplorer = (userAgent = ''): boolean => {
+    const ua = userAgent;
+    const msie = ua.indexOf('MSIE ');
+
+    if (msie > 0 || !!userAgent.match(/Trident.*rv\:11\./)) {
+        // If Internet Explorer, return version number
+        return true;
+    } // If another browser, return 0
+    else {
+        return false;
+    }
+};
+
+export const isSafari = () => {
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    return isSafari;
+};
+
+const overwriteMerge = (destinationArray: any, sourceArray: any, options: any) => sourceArray;
+
+export const mergeObjectWithArrayOverWrite = (...data: Record<string, unknown>[]) => {
+    return merge.all([...data], { arrayMerge: overwriteMerge });
+};
+
+export const checkUnModifiedFields = (originalObject: Record<string, unknown>, newData: Record<string, unknown>) => {
+    const result = JSON.parse(JSON.stringify(newData));
+
+    Object.keys(result).forEach((key) => {
+        if (JSON.stringify(originalObject[key]) === JSON.stringify(newData[key])) {
+            delete result[key];
+        }
+    });
+
+    return result;
+};
+
+export const trimArray = (arrayData: unknown[], reserveNumber: number) => {
+    if (arrayData.length > reserveNumber) {
+        arrayData = arrayData.filter((item, index) => {
+            if (index < reserveNumber) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+
+    return arrayData;
 };
